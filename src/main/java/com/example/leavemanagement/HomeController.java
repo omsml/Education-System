d1db793af706e3088c5b19baa7c03d643393f800
Omm Prakash Samal <ommprakashsamal9@gmail.com>
2026-03-07 20:37:13 +0530

    Remove HomeController class and its methods

--- src/main/java/com/example/leavemanagement/HomeController.java
+++ src/main/java/com/example/leavemanagement/HomeController.java
@@ -1,14 +1,1 @@
-package com.example.leavemanagement;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class HomeController {
-
-    @GetMapping("/")
-    public String index() {
-    return "index_page"; // No longer conflicts with a "/home" URL
-    }
-
-}
